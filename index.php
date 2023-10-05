092921de70481c113eff634e97d1f343b2c6d5ed
Daniel Anderson <dande396@bruinmail.slcc.edu>
2023-10-05 17:26:51 -0600

    Version 1.075
    Started Weather App, can't get API to call so commenting it out for now, minor change in To Do List

--- index.php
+++ index.php
@@ -264,6 +264,22 @@
           </div>
         </div>
         <!-- Portfolio Item End -->
+
+        <!-- Portfolio Item -->
+                <!-- Will Work On Later <div class="col mb-4">
+          <div class="card bg-light text-center border-light shadow h-100">
+            <div class="card-body">
+              <h3 class="card-title">Weather App</h3>
+              <hr class="bg-primary" />
+              <p class="card-text">A simple weather app.</p>
+            </div>
+            <div class="card-footer">
+              <a class="btn btn-outline-primary btn-lg mt-2" role="button" href="/weatherapp.html" target="_blank">Weather App</a>
+            </div>
+          </div>
+        </div> -->
+        <!-- Portfolio Item End -->
+
       </div>
     </div><br>
 
--- script.js
+++ script.js
@@ -143,3 +143,69 @@ myToDoList = new ToDo_Class(listSection);
 document.querySelector(".addBtn").addEventListener("click", function() {
     myToDoList.add()
 })
+
+// Weather App functions
+// Variables
+const searchInput = document.querySelector("#searchInput") 
+searchButton = document.querySelector("#searchButton") 
+weatherIcon = document.querySelector("#weatherIcon") 
+windSpeed = document.querySelector("#windSpeed") 
+humidity = document.querySelector(".humidity") 
+weather = document.querySelector(".weather") 
+desc = document.querySelector(".desc")
+API = "8cf5ac5621c8d0266298a149e49d7514";
+
+// Weather Details
+const setWeatherDetails = (data) => {
+    desc.innerHTML = data.weather[0].description;
+    weather.innerHTML = Math.round(data.main.temp - 273.15) + "°c";
+    humidity.innerHTML = data.main.humidity + "%";
+    windSpeed.innerHTML = data.wind.speed + "km/h";
+    switch (data.weather[0].main) {
+        case 'Clouds':
+            weatherIcon.src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.onlinewebfonts.com%2Fsvg%2Fimg_46485.png&f=1&nofb=1&ipt=c8909bf333b11e5bf52be2ed02af2e9f973978589538880ac7c771e38b0bd7f3&ipo=images";
+            break;
+        case 'Clear':
+            weatherIcon.src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.onlinewebfonts.com%2Fsvg%2Fimg_540148.png&f=1&nofb=1&ipt=13500cf99276c919117eb0b96335ac5711b22b7f55203986c7529db1b4e60f16&ipo=images";
+            break;
+        case 'Rain':
+            weatherIcon.src = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcdn.onlinewebfonts.com%2Fsvg%2Fimg_411702.png&f=1&nofb=1&ipt=dfaa5bda7945d38f65b2c5f753fbea71d1095082456bceb39a0a7df50e489c99&ipo=images";
+            break;
+        case 'Mist':
+            weatherIcon.src = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcdn.onlinewebfonts.com%2Fsvg%2Fimg_540402.png&f=1&nofb=1&ipt=47b219641f96d888ee081b8a3e197fb9476809a595a07a55b77e5b4bff34696d&ipo=images";
+            break;
+        case 'Snow':
+            weatherIcon.src = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcdn.onlinewebfonts.com%2Fsvg%2Fimg_541730.png&f=1&nofb=1&ipt=8057e2ab8fb7dd9b7fd89730824f4e8dd2c80d06b90e784d384c5e30d278555d&ipo=images";
+            break;
+        case 'Haze':
+            weatherIcon.src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.onlinewebfonts.com%2Fsvg%2Fimg_354824.png&f=1&nofb=1&ipt=1fb6a2bce080e1c388a77a53f7e96580c1dcffd2f036f80edca34ee79d743600&ipo=images";
+            break;
+
+    }
+}
+
+// Call API
+const callAPI = (id) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${id}`)
+        .then(response => {
+            // indicates whether the response is successful (status code 200-299) or not
+            if (!response.ok) {
+                alert("Check spelling of City and try again or Something Went Wrong!");
+                throw new Error(`Request failed with status ${response.status}`)
+            }
+            return response.json()
+        })
+        .then(data => {
+            setWeatherDetails(data);
+        })
+        .catch(error => console.log(error))
+}
+
+searchButton.addEventListener("click", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        searchButton.click();
+    }
+})
+
+searchButton.click();
