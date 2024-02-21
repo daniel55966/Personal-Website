5e6c71dd9c5446053399da0baad0f718da514d44
daniel55966 <daniel55966@gmail.com>
2024-02-21 13:30:16 -0700

    Version 1.0931
    WHOOPS FORGOT TO CHANGE A CARD TITLE

--- index.php
+++ index.php
@@ -372,13 +372,13 @@ if (($_SERVER["REQUEST_METHOD"] == "POST") && (!($formErr))) {
                 <div class="col mb-4">
           <div class="card bg-light text-center border-light shadow h-100">
             <div class="card-body">
-              <h3 class="card-title">Recipe App</h3>
+              <h3 class="card-title">Shopping Cart App</h3>
               <hr class="bg-primary" />
               <p class="card-text">A Shopping Cart App built using React, Redux, and TailwindCSS.</p>
             </div>
             <div class="card-footer">
               <a class="btn btn-outline-primary btn-lg mt-2" role="button" href="https://daniel55966.github.io/shopping-cart-app/" target="_blank">Shopping Cart App</a>
-            </div>
+            </div>  
           </div>
         </div>
         <!-- Portfolio Item End -->
