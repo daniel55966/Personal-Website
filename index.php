54bd311de0d919f89c7cd9e2b47e44ecb7d96e55
daniel55966 <daniel55966@gmail.com>
2025-07-15 13:28:51 -0600

    Version 1.1323
    Built a Spinning Wheel Random Picker using React and the style-components library.

--- index.php
+++ index.php
@@ -593,6 +593,21 @@ if (($_SERVER["REQUEST_METHOD"] == "POST") && (!($formErr))) {
         </div>
         <!-- Portfolio Item End -->
 
+        <!-- Portfolio Item -->
+        <div class="col mb-4">
+          <div class="card bg-light text-center border-light shadow h-100">
+            <div class="card-body">
+              <h3 class="card-title">Spinning Wheel Random Picker</h3>
+              <hr class="bg-primary" />
+              <p class="card-text">A simple spinning wheel picker app built using React and the styled-components library.</p>
+            </div>
+            <div class="card-footer">
+              <a class="btn btn-outline-primary btn-lg mt-2" role="button" href="https://wheel-picker-nine.vercel.app/" target="_blank">Spinning Wheel Random Picker</a>
+            </div>
+          </div>
+        </div>
+        <!-- Portfolio Item End -->
+
       </div>
     </div><br>
 
