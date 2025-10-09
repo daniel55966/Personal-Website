dd77b51f25e143d0ae1645ff0a25bd6b04b3c575
daniel55966 <daniel55966@gmail.com>
2025-10-09 13:36:09 -0600

    Version 1.1326
    Added a Simple Code Explainer to the portfolio built using React 19 and the gpt-oss-120b LLM

--- index.php
+++ index.php
@@ -638,6 +638,21 @@ if (($_SERVER["REQUEST_METHOD"] == "POST") && (!($formErr))) {
         </div>
         <!-- Portfolio Item End -->
 
+        <!-- Portfolio Item -->
+        <div class="col mb-4">
+          <div class="card bg-light text-center border-light shadow h-100">
+            <div class="card-body">
+              <h3 class="card-title">Simple Code Explainer</h3>
+              <hr class="bg-primary" />
+              <p class="card-text">A simple Code Explainer for JavaScript, Python, and Java built using React 19 and using the "gpt-oss-120b" LLM.</p>
+            </div>
+            <div class="card-footer">
+              <a class="btn btn-outline-primary btn-lg mt-2" role="button" href="https://ai-code-explainer-sepia.vercel.app/" target="_blank">Simple Code Explainer</a>
+            </div>
+          </div>
+        </div>
+        <!-- Portfolio Item End -->
+
       </div>
     </div><br>
 
